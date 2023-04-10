6a70fa30768d69dc36fd60e12761308e43fb6084
Miloš Čadek <34865543+c4da@users.noreply.github.com>
2023-04-10 20:39:47 +0200

    Create Day_12_java8.java

---
+++ Day_12_java8.java
@@ -0,0 +1,97 @@
+/*
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, .
+A string, .
+An integer, .
+An integer array (or vector) of test scores, .
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+*/
+
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    public Student(String firstName, String lastName, int id, int[] scores){
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    public char calculate(){
+        int sum = 0;
+        for (int score : this.testScores){
+            sum += score;
+        }
+        float a = (float) sum / (float) this.testScores.length;
+        if (a >= 90 && a <= 100){
+            return 'O';
+        } else if (a >= 80 && a <= 90){
+            return 'E';
+        } else if (a >= 70 && a <= 80){
+            return 'A';
+        } else if (a >= 55 && a <= 70){
+            return 'P';
+        } else if (a >= 40 && a <= 55){
+            return 'D';
+        } else {
+            return 'T';
+        }
+    }
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
